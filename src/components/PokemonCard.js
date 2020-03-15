dcbbf035236c3dbaae5faf608090edd86afdc69e
Stanley Chen <dannyshien@gmail.com>
2020-03-14 21:54:42 -0400

    Create container folder and move some files inot folder

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from './node_modules/react'; 
 import './PokemonCard.css';
 
 const PokemonCard = ({pokemonInfo, pokemonName, pokemonSprite, pokemonTypes}) => {
